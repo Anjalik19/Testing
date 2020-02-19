5835c79155da7716fc2a72a72993d3c423a16264
Anjalik19 <Anjali.march19@gmail.com>
2020-02-19 11:13:53 +0530

    [Anjali]Refactor. refactor logic of regex in registration form

--- XUnitTesting/Regestration.cs
+++ XUnitTesting/Regestration.cs
@@ -7,60 +7,155 @@ namespace XUnitTesting
 {
     class Regestration
     {
+        //Regex Condition for first name when value is not null
         public bool FirstName(string FName)
         {
             var FirstNameRegex = "^[a-zA-Z].{4,8}$";
             Regex reg = new Regex(FirstNameRegex);
-            if (reg.IsMatch(FName))
+            if (FName != null)
             {
-                return true;
+                if (reg.IsMatch(FName))
+                {
+
+                    return true;
+                }
             }
-            else
-            
-                return false;
-
+            return false;
         }
-    
 
+        //Regex Condition for first name when value is null
+        public bool fname(string name1)
+        {
+            var FirstNameRegex = "^[a-zA-Z].{4,8}$";
+            Regex reg = new Regex(FirstNameRegex);
+            if (name1==null)
+            {
+                if (reg.IsMatch(name1))
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        //Regex Condition for last name when value is not null
         public bool LastName(string LName)
         {
             var LNameRegex = "^[a-zA-Z].{4,8}$";
             Regex reg = new Regex(LNameRegex);
-            if (reg.IsMatch(LName))
+            if (LName != null)
             {
-                return true;
-            }
-            return false;
-        }
-        public bool password(string pass)
-        {
-            var passRegex = "^[a-zA-Z0-9].{4,8}$";
-            Regex reg = new Regex(passRegex);
-            if(reg.IsMatch(pass))
-            {
-                return true;
-            }
-            return false;
-        }
-        public bool PhNum(string phNum)
-        {
-            var numRegex = "^[0-9]*$";
-            Regex reg = new Regex(numRegex);
-            if(reg.IsMatch(phNum))
-            {
-                return true;
+                if (reg.IsMatch(LName))
+                {
+                    return true;
+                }
             }
             return false;
         }
 
-        public  bool Email(string inputEmail)
+        //Regex Condition for first name when value is  null
+        public bool lname(string name2)
+        {
+            var LNameRegex = "^[a-zA-Z].{4,8}$";
+            Regex reg = new Regex(LNameRegex);
+            if (name2 == null)
+            {
+                if (reg.IsMatch(name2))
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        //Regex Condition for password when value is not null
+        public bool password(string pass)
+        {
+            var passRegex = "^[a-zA-Z0-9].{4,8}$";
+            Regex reg = new Regex(passRegex);
+            if (pass != null)
+            {
+                if (reg.IsMatch(pass))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Regex Condition for password when value is  null
+        public bool passwrd(string pass1)
+        {
+            var passRegex = "^[a-zA-Z0-9].{4,8}$";
+            Regex reg = new Regex(passRegex);
+            if (pass1 == null)
+            {
+                if (reg.IsMatch(pass1))
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        //Regex Condition for phone number when value is not null
+        public bool PhNum(string phNum)
+        {
+            var numRegex = "^[0-9]*$";
+            Regex reg = new Regex(numRegex);
+            if (phNum != null)
+            {
+                if (reg.IsMatch(phNum))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Regex Condition for phone number when value is null
+        public bool mobile(string num)
+        {
+            var numRegex = "^[0-9]*$";
+            Regex reg = new Regex(numRegex);
+            if (num == null)
+            {
+                if (reg.IsMatch(num))
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+        //Regex Condition for Email when value is not null
+        public bool Email(string inputEmail)
         {
             string emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{1,5}";
             Regex reg = new Regex(emailRegex);
-            if (reg.IsMatch(inputEmail))
-                return true;
-            else
+            if (inputEmail != null)
+            {
+                if (reg.IsMatch(inputEmail))
+                {
+                    return true;
+                }
+            }
                 return false;
+        }
+
+        //Regex Condition for Email when value is null
+        public bool mail(string email)
+        {
+            string emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{1,5}";
+            Regex reg = new Regex(emailRegex);
+            if (email == null)
+            {
+                if (reg.IsMatch(email))
+                {
+                    return true;
+                }
+            }
+            return true;
         }
     }
 }  
--- XUnitTesting/Testing.cs
+++ XUnitTesting/Testing.cs
@@ -8,31 +8,75 @@ namespace XUnitTesting
     {
         Regestration r = new Regestration();
 
+        //Using assert for first name when value is not null
         [Fact]
         public void firstName()
         {
             Assert.True(r.FirstName("Anjali"));
            
         }
+
+        //Using assert for first name when value is null
+        [Fact]
+        public void FNull()
+        {
+            Assert.True(r.fname(" "));
+        }
+
+        //Using assert for last name when value is not null
         [Fact]
         public void lastName()
         {
             Assert.True(r.LastName("AAAbkad"));
         }
+
+        //Using assert for last name when value is null
+        [Fact]
+        public void LNull()
+        {
+            Assert.True(r.lname(" "));
+        }
+
+        //Using assert for password when value is not null
         [Fact]
         public void Password()
         {
             Assert.True(r.password("anjaALI3"));
         }
+
+        //Using assert for password when value is null
+        [Fact]
+        public void Passwrd()
+        {
+            Assert.True(r.passwrd(" "));
+        }
+
+        //Using assert for phone number when value is not null
         [Fact]
         public void Number()
         {
             Assert.True(r.PhNum("33720023"));
         }
+
+        //Using assert for phone number when value is null
+        [Fact]
+        public void number()
+        {
+            Assert.True(r.mobile(" "));
+        }
+
+        //Using assert for email when value is not null
         [Fact]
         public void EMail()
         {
             Assert.True(r.Email("anjaliK19@gmail.com"));
+        }
+
+        //Using assert for email when value is null
+        [Fact]
+        public void Email()
+        {
+            Assert.True(r.mail(" "));
         }
     }
 }
